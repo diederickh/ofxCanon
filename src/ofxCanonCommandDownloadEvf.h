9aed446adf58fc5bd3bfec139c599f8b9e967433
roxlu <diederick@apollomedia.nl>
2010-09-25 17:39:25 +0200

    Update

--- src/ofxCanonCommandDownloadEvf.h
+++ src/ofxCanonCommandDownloadEvf.h
@@ -28,7 +28,7 @@ public:
 		EdsEvfImageRef evf_image = NULL;
 		EdsStreamRef stream = NULL;
 		EdsUInt32 buffer_size = 2 * 1024 * 1024;
-	
+		std::cout << "evf download\n" ;
 		// Exit unless during live view
 		if ((model->getEvfOutputDevice() & kEdsEvfOutputDevice_PC) == 0) {
 			return true;
